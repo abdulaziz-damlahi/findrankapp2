e6cd3e76c90cf46151c30adab502da2ade6e8e93
baris celiker <td21brs14@hotmail.com>
2021-03-04 14:49:16 +0300

    Merge remote-tracking branch 'origin/dev-abdulaziz' into baris_merge
    # Conflicts:
    #	.idea/deployment.xml
    #	.idea/findRankApp.iml
    #	.idea/php.xml
    #	app/Http/Controllers/homepage.php
    #	app/Models/keywords.php
    #	app/Models/packets.php
    #	app/Models/websites.php
    #	database/migrations/2021_03_01_153224_create_websites_table.php
    #	database/migrations/2021_03_01_153453_create_keywords_table.php
    #	public/css/font-awesome.min.css
    #	public/css/style.css
    #	public/js/main.js
    #	resources/views/layouts/header.blade.php
    #	routes/web.php

---
+++ app/Http/Controllers/panel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\packets;
+use App\Models\User;
+use App\Models\websites;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class panel extends Controller
+{
+    public function index()
+    {
+        return view(
+            'pages/panel/panel');
+    }
+
+    public function profile()
+    {
+        return view(
+            'pages/panel/profile');
+    }
+    public function FindOrder()
+    {
+        return view(
+            'pages/findorder');
+    }
+
+    public function userspacket()
+    {
+        $user = User::find(2);
+        $user->packets;
+        return $user;
+    }
+
+    public function userswebsite()
+    {
+        $user = User::find(1);
+         $user->websites;
+        return $user;
+    }
+
+    public function packetwebsite()
+    {
+        $packet = packets::find(1);
+         $packet->websites;
+        return $packet;
+    }
+
+    public function websitekeyword()
+    {
+        $website = websites::find(1);
+         $website->keywords;
+        return $website;
+    }
+}
