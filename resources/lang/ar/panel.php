04513abe7a710c75162e0a444cf3a4aa0b1e10ff
abdulaziz damlahi2 <abooddamlakhi@gmail.com>
2021-04-08 09:27:59 +0300

    no message

---
+++ resources/lang/ar/panel.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'different'=>'GÜNLÜK DEĞİŞİM',
+    'Add Website'=>'Site Ekle',
+    'website'=>'website',
+    'word'=>'kelime',
+    'rank'=>'rank',
+    'graph'=>'grafik',
+    'positions'=>'Anahtar Kelime Pozisyonları',
+    'Statistics'=>'İstatistik',
+    'first'=>'ilk',
+    'de'=>' de',
+    'Followed Websites'=>'Takip Edilen Web Siteleri',
+    'Keywords Used'=>'Kullanılan Anahtar Kelimeler',
+    'order'=>'sıra',
+    'keyword'=>'anahtar kelime',
+    'Sites'=>'Sites',
+    'daily change'=>'günlük değişim',
+    'delete'=>'sil',
+    'words count'=>'kelime sayı',
+    'save'=>'Kaydet',
+    'close'=>'Kapat',
+];
+?>
